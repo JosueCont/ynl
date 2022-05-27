c0369e100fe1f77c35736f98947480b5ffbd279d
eduardocouoh <eduardo.couoh@hiumanlab.com>
2022-05-26 22:07:36 -0500

    * Historial maquetacion detalles.

--- screens/HistoryFeelingScreenDetail.js
+++ screens/HistoryFeelingScreenDetail.js
@@ -1,61 +1,88 @@
 import React, {useEffect, useState} from "react";
-import {ScrollView, TouchableOpacity} from "react-native";
+import {ScrollView} from "react-native";
 import {connect} from "react-redux";
-import {Box, Button, Checkbox, FormControl, Icon, Image, Input, Stack, Text, useToast, View} from "native-base";
-import {Entypo, MaterialIcons} from "@expo/vector-icons";
+import {Image, Skeleton, Stack, Text, View} from "native-base";
 import {Colors} from "../utils/Colors";
-import ApiApp from "../utils/ApiApp";
 import {useIsFocused} from "@react-navigation/native";
 import {getShadowCircleStyle} from "../utils/functions";
-import bg1 from "../assets/bg1.png";
 import _ from "lodash";
 import moment from 'moment'
-import apiApp from "../utils/ApiApp";
 
 
-const HistoryFeelingScreenDetail = ({authDuck, route,navigation}) => {
+const HistoryFeelingScreenDetail = ({authDuck, route, navigation}) => {
 
     const isFocused = useIsFocused();
-    const [detailData, setDetailData] = useState(null)
-    const { detail, date } = route.params;
+    const [detailData, setDetailData] = useState([])
+    const {detail, date} = route.params;
+    const [loading, setLoading] = useState(null);
 
     useEffect(() => {
+
         if (isFocused) {
-            setDetailData(_.orderBy(detail,['date'],['desc']))
+            setLoading(true)
+            setDetailData(_.orderBy(detail, ['date'], ['desc']))
+            setTimeout(() => {
+                setLoading(false)
+            }, 200)
 
         }
     }, [isFocused])
 
 
-
-
     return (
-        <ScrollView style={{flex: 1}} contentContainerStyle={{backgroundColor: Colors.white}}>
+        <ScrollView style={{flex: 1}} contentContainerStyle={{backgroundColor: Colors.white}} bounces={false}>
 
             <View flex={1} alignItems={'center'}>
                 <Stack space={4} w="90%">
-                    <Text style={{fontSize:20, color:'#FF5E00', textAlign:'center',fontWeight:'bolder'}}>{date}</Text>
+                    <Text style={{
+                        fontSize: 20,
+                        color: '#FF5E00',
+                        textAlign: 'center',
+                        fontWeight: 'bolder'
+                    }}>{moment(date, 'DD/MM/YYYY').format('LL')}</Text>
 
                     {
-                        detailData && detailData.map((ele,i)=>{
-                            return  <View
-                                flexDir={'row'}
-                                mb={2}
-                                bgColor={'#'+_.get(ele,'feeling.attributes.parent.data.attributes.parent.data.attributes.color','5F6367')}
-                                style={getShadowCircleStyle(10, 10)}
-                                borderRadius={40}
-                                p={3}>
-                                <View flex={1} height={70} alignItems={'center'} justifyContent={'center'}>
-                                    <View style={{backgroundColor:'white'}} p={2} borderRadius={40}>
-                                        <Image alt=":)" size="sm" source={{uri:`${_.get(ele,'feeling.attributes.parent.data.attributes.parent.data.attributes.icon.data.attributes.url','https://app-ynl.s3.us-west-1.amazonaws.com/triste_4a4900f0cd.png')}`}}/>
-                                    </View>
-                                </View>
-                                <View flex={2} height={70} mr={1}>
-                                        <Text fontSize={26} style={{color:'white'}} >{ele.feeling.attributes.name}</Text>
-                                        <Text fontSize={16}>{moment(ele.date).format('HH:mm')}</Text>
-                                </View>
+                        loading ?
+                            <View flex={1}>
+                                <Skeleton height={20} mb={4}/>
+                                <Skeleton height={20} mb={4}/>
+                                <Skeleton height={20} mb={4}/>
+                                <Skeleton height={20} mb={4}/>
+                                <Skeleton height={20} mb={4}/>
+                                <Skeleton height={20} mb={4}/>
+                                <Skeleton height={20} mb={4}/>
+
                             </View>
-                        })
+
+                            :
+                            detailData.map((ele, i) => {
+                                return (
+
+                                    <View
+                                        flexDir={'row'}
+                                        mb={2}
+                                        bgColor={'#' + _.get(ele, 'feeling.attributes.parent.data.attributes.parent.data.attributes.color', '5F6367')}
+                                        style={getShadowCircleStyle(10, 10)}
+                                        borderRadius={10}
+                                        py={3}
+
+                                    >
+                                        <View flex={0.5} alignItems={'center'} justifyContent={'center'}>
+                                            <Image tintColor={'white'} style={{height: 40, width: 40}}
+                                                   source={{uri: `${_.get(ele, 'feeling.attributes.parent.data.attributes.parent.data.attributes.icon.data.attributes.url', 'https://app-ynl.s3.us-west-1.amazonaws.com/triste_4a4900f0cd.png')}`}}/>
+                                        </View>
+                                        <View flex={1} justifyContent={'center'}>
+                                            <Text color={'white'} style={{fontWeight: 'bold'}}
+                                                  fontSize={20}>{ele.feeling.attributes.name} </Text>
+
+                                            <Text color={'white'} fontSize={16}
+                                                  mb={1}>{ele.feeling.attributes.parent.data.attributes.name} - {ele.feeling.attributes.parent.data.attributes.parent.data.attributes.name}</Text>
+                                            <Text color={'white'}
+                                                  fontSize={9}>{moment(ele.date).format('hh:mm A')}</Text>
+                                        </View>
+                                    </View>
+                                )
+                            })
                     }
 
 
