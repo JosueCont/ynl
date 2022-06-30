1ebd7ae630ca9d95955d221f908d2dcba3984dff
eduardocouoh <eduardo.couoh@hiumanlab.com>
2022-06-30 18:20:44 -0500

    * Validacion actualizar contraseña

--- screens/Modals/ModalPasswordUpdate.js
+++ screens/Modals/ModalPasswordUpdate.js
@@ -4,11 +4,30 @@ import {styles} from "./ModalStyleSheet";
 import {Button, FormControl, Icon, Input, Text, View} from "native-base";
 import {MaterialIcons} from "@expo/vector-icons";
 import {Colors} from "../../utils/Colors";
+import {useFormik} from "formik";
+import * as Yup from "yup";
 
 const ModalPasswordUpdate = ({visible, setVisible, action}) => {
     const [oldPassword, setOldPassword] = useState(null)
     const [password, setPassword] = useState(null)
     const [passwordConfirm, setPasswordConfirm] = useState(null)
+
+    const formik = useFormik({
+        initialValues: {
+            oldPassword: '',
+            password: '',
+            repeatPassword: ''
+        },
+        onSubmit: (formValue) => {
+            action(formValue.oldPassword, formValue.password, formValue.repeatPassword)
+        },
+        validateOnChange: false,
+        validationSchema: Yup.object({
+            oldPassword: Yup.string().required("La contraseña es obligatoria"),
+            password: Yup.string().required("La contraseña es obligatoria").min(8, "La contraseña debe ser de al menos 8 caracteres."),
+            repeatPassword: Yup.string().required("La contraseña es obligatoria").oneOf([Yup.ref('password')], "Las contraseñas tienen que ser iguales."),
+        })
+    });
 
     return (
         <Modal
@@ -30,35 +49,72 @@ const ModalPasswordUpdate = ({visible, setVisible, action}) => {
                         <Text textAlign={'center'} color={Colors.red} fontSize={22} mb={4}>Actualizar contraseña</Text>
                     </View>
                     <View p={5} width={'100%'}>
-                        <FormControl isInvalid w="100%">
-                            <Input type="password" onChangeText={val => setOldPassword(val)} value={oldPassword}
-                                   size={'md'} height={50}
-                                   mb={4} placeholder={'Contraseña anterior'} borderRadius={25}
-                                   placeholderTextColor={Colors.red} textAlign={'center'}
+                        <FormControl isInvalid={formik.errors.oldPassword} mb={2}>
+                            <Input
+                                type="password"
+                                onChangeText={val => {
+                                    formik.setFieldValue('oldPassword', val)
+                                }}
+                                //value={oldPassword}
+                                size={'md'}
+                                height={50}
+                                placeholder={'Contraseña anterior'}
+                                borderRadius={25}
+                                placeholderTextColor={Colors.red}
+                                textAlign={'center'}
                             />
-                            <Input type="password" onChangeText={val => setPassword(val)} value={password} size={'md'}
-                                   height={50}
-                                   mb={4} placeholder={'Contraseña nueva'} borderRadius={25}
-                                   placeholderTextColor={Colors.red} textAlign={'center'}
-                            />
-                            <Input type="password" onChangeText={val => setPasswordConfirm(val)} value={passwordConfirm}
-                                   size={'md'}
-                                   height={50} mb={4} placeholder={'Confirmar contraseña nueva'}
-                                   borderRadius={25}
-                                   placeholderTextColor={Colors.red} textAlign={'center'}
-                            />
-                            <View flexDir={'row'} mt={4}>
-                                <View flex={1} mr={1}>
-                                    <Button size={'sm'} colorScheme={'orange'} onPress={() => {
-                                        setVisible(false)
-                                    }}>Cancelar</Button>
-                                </View>
-                                <View flex={1} ml={1}>
-                                    <Button size={'sm'} colorScheme={'orange'}
-                                            onPress={() => action(oldPassword, password, passwordConfirm)}>Actualizar</Button>
-                                </View>
-                            </View>
+                            <FormControl.ErrorMessage>
+                                {formik.errors.oldPassword}
+                            </FormControl.ErrorMessage>
                         </FormControl>
+                        <FormControl isInvalid={formik.errors.password} mb={2}>
+                            <Input
+                                type="password"
+                                onChangeText={val => {
+                                    formik.setFieldValue('password', val)
+                                }}
+                                size={'md'}
+                                height={50}
+
+                                placeholder={'Contraseña nueva'}
+                                borderRadius={25}
+                                placeholderTextColor={Colors.red}
+                                textAlign={'center'}
+                            />
+                            <FormControl.ErrorMessage>
+                                {formik.errors.password}
+                            </FormControl.ErrorMessage>
+                        </FormControl>
+                        <FormControl isInvalid={formik.errors.repeatPassword} mb={2}>
+
+                            <Input type="password"
+                                   onChangeText={val => {
+                                       formik.setFieldValue('repeatPassword', val)
+                                   }}
+                                   size={'md'}
+                                   height={50}
+                                   placeholder={'Confirmar contraseña nueva'}
+                                   borderRadius={25}
+                                   placeholderTextColor={Colors.red}
+                                   textAlign={'center'}
+                            />
+                            <FormControl.ErrorMessage>
+                                {formik.errors.repeatPassword}
+                            </FormControl.ErrorMessage>
+                        </FormControl>
+                        <View flexDir={'row'} mt={4}>
+                            <View flex={1} mr={1}>
+                                <Button size={'sm'} colorScheme={'orange'} onPress={() => {
+                                    setVisible(false)
+                                }}>Cancelar</Button>
+                            </View>
+                            <View flex={1} ml={1}>
+                                <Button size={'sm'} colorScheme={'orange'}
+                                        onPress={() => {
+                                            formik.handleSubmit();
+                                        }}>Actualizar</Button>
+                            </View>
+                        </View>
                     </View>
                 </View>
             </View>
