487dc7532e8eb8ac0c5ec29b4eb28c441eb60d70
eduardocouoh <eduardo.couoh@hiumanlab.com>
2022-07-06 10:54:21 -0500

    * Compilaciones.

--- app.config.js
+++ app.config.js
@@ -33,14 +33,14 @@ const commonConfig = {
 };
 
 const ios = {
-    buildNumber: "22",
+    buildNumber: "23",
     supportsTablet: false,
     bundleIdentifier: "com.hiumanlab.ynl",
     usesAppleSignIn: true,
 };
 
 const android = {
-    versionCode: 16,
+    versionCode: 17,
     adaptiveIcon: {
         foregroundImage: "./assets/adaptive-icon.png",
         backgroundImage: "./assets/adaptive-icon-bg.png"
