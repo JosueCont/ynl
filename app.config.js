b082eb3656f57853cdb2761628294c02a5a3cd59
Gaspar Dzul <casper088@gmail.com>
2023-10-30 15:41:09 -0600

    ajustes bugfixes registro

--- app.config.js
+++ app.config.js
@@ -46,14 +46,14 @@ const commonConfig = {
 };
 
 const ios = {
-    buildNumber: "46",
+    buildNumber: "47",
     supportsTablet: false,
     bundleIdentifier: "com.hiumanlab.ynl",
     usesAppleSignIn: true,
 };
 
 const android = {
-    versionCode: 46,
+    versionCode: 47,
     adaptiveIcon: {
         foregroundImage: "./assets/adaptive-icon.png",
         backgroundImage: "./assets/adaptive-icon-bg.png"
@@ -71,21 +71,21 @@ module.exports = () => {
         return {
             ...commonConfig,
             slug: "ynl",
-            version: "2.1.3",
+            version: "2.1.4",
             ios: ios,
         };
     } else if (process.env.APP_ENV === "android") {
         return {
             ...commonConfig,
             slug: "ynl",
-            version: "2.1.3",
+            version: "2.1.4",
             android: android
         };
     } else if (process.env.APP_ENV === "expo") {
         return {
             ...commonConfig,
             slug: "ynl-dev",
-            version: "2.1.3",
+            version: "2.1.4",
             ios: ios,
             android: android,
         };
@@ -93,7 +93,7 @@ module.exports = () => {
         return {
             ...commonConfig,
             slug: "ynl",
-            version: "2.1.3",
+            version: "2.1.4",
             android: android,
             ios: ios,
         };
